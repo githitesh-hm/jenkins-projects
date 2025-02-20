e5298bbd3e17d9aa4237357e8adfc88eb46b2ad9
githitesh-hm <hiteshmore0706@gmail.com>
2025-02-20 09:46:33 +0530

    initial commit

---
+++ log_upload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Variables
+LOG_DIR="/var/log/apache2"
+S3_BUCKET="your-log-bucket-name"
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+
+# Compress logs
+tar -czf /tmp/apache-logs-$TIMESTAMP.tar.gz -C $LOG_DIR access.log error.log
+
+# Upload logs to S3
+aws s3 cp /tmp/apache-logs-$TIMESTAMP.tar.gz s3://$S3_BUCKET/
+
+# Cleanup local compressed file
+rm -f /tmp/apache-logs-$TIMESTAMP.tar.gz
