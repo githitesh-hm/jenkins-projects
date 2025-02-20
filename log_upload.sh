3486b84fc358be144aeaa8c26f9881c1f3b57422
githitesh-hm <hiteshmore0706@gmail.com>
2025-02-20 10:25:05 +0530

    Update log_upload.sh

--- log_upload.sh
+++ log_upload.sh
@@ -9,7 +9,7 @@ TIMESTAMP=$(date +%Y%m%d%H%M%S)
 tar -czf /tmp/apache-logs-$TIMESTAMP.tar.gz -C $LOG_DIR access.log error.log
 
 # Upload logs to S3
-aws s3 cp /tmp/apache-logs-$TIMESTAMP.tar.gz s3://$S3_BUCKET/
+sudo aws s3 cp /tmp/apache-logs-$TIMESTAMP.tar.gz s3://$S3_BUCKET/
 
 # Cleanup local compressed file
 rm -f /tmp/apache-logs-$TIMESTAMP.tar.gz
